8305dc5aec7fa499816880227de48b4cf9ab2112
Nirmal Mudaliar <Nirmal Mudaliar@DESKTOP-OKKKOBD>
2023-01-08 12:02:32 +0530

    Initial setup done with DI

---
+++ Currency Converter/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_Converter.ViewModels
+{
+    public class BaseViewModel: Conductor<object>
+    {
+        private HomeViewModel _homeViewModel;
+        public BaseViewModel(HomeViewModel homeViewModel) {
+            _homeViewModel = homeViewModel;
+            ActivateItemAsync(_homeViewModel);
+        }
+    }
+}
---
+++ Currency Converter/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_Converter.ViewModels
+{
+    public class HomeViewModel: Conductor<object>
+    {
+        public HomeViewModel()
+        {
+
+        }
+    }
+}
